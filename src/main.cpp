6ee4f1131d7a57fa06bfb638eea55ecc95dcfbc2
blighli <blighli@gmail.com>
2013-08-11 16:07:57 +0800

    add tesseract and leptonica

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
 
 int main(int argc, char** argv)
 {
 	QCoreApplication app(argc, argv);
 	qDebug()<<"hello qt!";
 	app.exec();
+	
+	char *outText;
+	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+	if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
 }
