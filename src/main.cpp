df803cc7bd9c9c9b27f427f5751237216b728578
blighli <blighli@gmail.com>
2013-08-11 16:51:38 +0800

    first runnable ocr program

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <QtCore/QCoreApplication>
+#include <QTextCodec>
 #include <QtCore/QDebug>
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
@@ -6,13 +7,26 @@
 int main(int argc, char** argv)
 {
 	QCoreApplication app(argc, argv);
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 	qDebug()<<"hello qt!";
-	app.exec();
 	
 	char *outText;
 	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-	if (api->Init(NULL, "eng")) {
+	if (api->Init(NULL, "sim")) {
         fprintf(stderr, "Could not initialize tesseract.\n");
         exit(1);
     }
+
+	Pix *image = pixRead("C:\\Users\\liqilei\\Desktop\\test3.png");
+    api->SetImage(image);
+    // Get OCR result
+    outText = api->GetUTF8Text();
+	qDebug()<<outText;
+    // Destroy used object and release memory
+    api->End();
+    //delete [] outText;
+    pixDestroy(&image);
+	
+	app.exec();
+    return 0;
 }
