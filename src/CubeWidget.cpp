9fa428bcd63ebdf8afc686f705f088f4e3925951
blighli <blighli@gmail.com>
2013-08-21 06:17:13 +0800

    add opengl support

--- src/CubeWidget.cpp
+++ src/CubeWidget.cpp
@@ -1,6 +1,22 @@
 #include "CubeWidget.h"
 
-CubeWidget::CubeWidget()
+CubeWidget::CubeWidget( QWidget *parent ) : QGLWidget(parent)
+{
+
+}
+
+void CubeWidget::initializeGL()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void CubeWidget::resizeGL( int w, int h )
+{
+	glViewport(0, 0, (GLint)w, (GLint)h);
+}
+
+void CubeWidget::paintGL()
 {
 
 }
--- src/CubeWidget.h
+++ src/CubeWidget.h
@@ -5,8 +5,15 @@
 
 class CubeWidget : public QGLWidget
 {
+	Q_OBJECT
 public:
-	CubeWidget();
+	CubeWidget(QWidget *parent = 0);
+
+protected:
+	void initializeGL();
+	void resizeGL(int w, int h);
+	void paintGL();
+
 };
 
 #endif //CUBEWIDGET_H
