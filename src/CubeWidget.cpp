b423ce46c6f2adbe6cacd44258ddc29d8ac0741b
blighli <blighli@gmail.com>
2013-08-21 06:08:36 +0800

    add glwidget

---
+++ src/CubeWidget.cpp
@@ -0,0 +1,6 @@
+#include "CubeWidget.h"
+
+CubeWidget::CubeWidget()
+{
+
+}
---
+++ src/CubeWidget.h
@@ -0,0 +1,12 @@
+#ifndef CUBEWIDGET_H
+#define CUBEWIDGET_H
+
+#include <QGLWidget>
+
+class CubeWidget : public QGLWidget
+{
+public:
+	CubeWidget();
+};
+
+#endif //CUBEWIDGET_H
