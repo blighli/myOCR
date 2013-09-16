e2d7e76c294ba344c4e7a7641257e3ca26b232e3
blighli <blighli@gmail.com>
2013-09-16 15:04:59 +0800

    add iOCR project: empty

---
+++ ios/iOCR/iOCR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOCR
+//
+//  Created by cstlab on 13-9-16.
+//  Copyright (c) 2013年 liqilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/iOCR/iOCR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOCR
+//
+//  Created by cstlab on 13-9-16.
+//  Copyright (c) 2013年 liqilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
